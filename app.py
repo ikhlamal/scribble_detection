eda5a93cd9deb1ec52c42d32b08c7e593f5a0fa1
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2026-01-12 05:32:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ def match_with_references(canvas, bbox, refs):
         return 0.0
     
     x1, y1, x2, y2 = bbox
-    margin = 80
+    margin = 95
     
     x1 = max(0, int(x1) - margin)
     y1 = max(0, int(y1) - margin)

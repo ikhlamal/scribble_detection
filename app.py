e8285d1bf44f838fa77e25bd69d9b3eee1e3457f
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2026-01-12 07:22:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ def is_stroke_complex(points, bbox_area, length):
     # Kriteria kompleksitas
     is_long = length > 300000 # stroke panjang
     is_large_area = bbox_area > 800000  # area besar
-    is_many_points = num_points > 200  # banyak titik
+    is_many_points = num_points > 20  # banyak titik
     
     # Kompleks jika memenuhi salah satu kriteria dengan margin
     return is_long or is_large_area or is_many_points
